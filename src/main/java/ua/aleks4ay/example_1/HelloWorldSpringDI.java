d903d7b8b7d8d38541370c9bdbdee0cd7892ec82
Aleksey <aleks4ay@gmail.com>
2021-04-20 19:18:02 +0300

    Hello world Spring-application (via Java config instead xml-file).

--- src/main/java/ua/aleks4ay/example_1/HelloWorldSpringDI.java
+++ src/main/java/ua/aleks4ay/example_1/HelloWorldSpringDI.java
@@ -1,11 +1,12 @@
 package ua.aleks4ay.example_1;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ua.aleks4ay.example_1.config.JavaConfig;
 
 public class HelloWorldSpringDI {
     public static void main(String[] args) {
-        ApplicationContext appContext = new ClassPathXmlApplicationContext("/WEB-INF/spring/app-context.xml");
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(JavaConfig.class);
         MessageRenderer mr = appContext.getBean("messageRenderer", MessageRenderer.class);
         mr.render();
     }
---
+++ src/main/java/ua/aleks4ay/example_1/config/JavaConfig.java
@@ -0,0 +1,8 @@
+package ua.aleks4ay.example_1.config;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("ua.aleks4ay")
+public class JavaConfig {
+
+}
