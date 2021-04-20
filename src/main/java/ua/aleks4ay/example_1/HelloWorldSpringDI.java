9156678a793683f32d8ae587f01a69985f8d3b52
Aleksey <aleks4ay@gmail.com>
2021-04-20 20:25:54 +0300

    Use GenericXmlApplicationContext.

--- src/main/java/ua/aleks4ay/example_1/HelloWorldSpringDI.java
+++ src/main/java/ua/aleks4ay/example_1/HelloWorldSpringDI.java
@@ -1,15 +1,13 @@
 package ua.aleks4ay.example_1;
 
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class HelloWorldSpringDI {
     public static void main(String[] args) {
-        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
-        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
-        reader.loadBeanDefinitions(new ClassPathResource("/WEB-INF/spring/app-context.xml"));
-        MessageRenderer mr = (MessageRenderer) factory.getBean("renderer");
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:/WEB-INF/spring/app-context.xml");
+        ctx. refresh ();
+        MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
         mr.render();
     }
 }
