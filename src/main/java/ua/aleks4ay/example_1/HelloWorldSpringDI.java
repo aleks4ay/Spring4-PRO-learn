c886af1646a5370abf5ee374ace9f2b87ea843b0
Aleksey <aleks4ay@gmail.com>
2021-04-20 19:34:22 +0300

    Use BeanFactory (instead ApplicationContext)

--- src/main/java/ua/aleks4ay/example_1/HelloWorldSpringDI.java
+++ src/main/java/ua/aleks4ay/example_1/HelloWorldSpringDI.java
@@ -1,12 +1,15 @@
 package ua.aleks4ay.example_1;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
 
 public class HelloWorldSpringDI {
     public static void main(String[] args) {
-        ApplicationContext appContext = new ClassPathXmlApplicationContext("/WEB-INF/spring/app-context.xml");
-        MessageRenderer mr = appContext.getBean("messageRenderer", MessageRenderer.class);
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+        reader.loadBeanDefinitions(new ClassPathResource("/WEB-INF/spring/app-context.xml"));
+        MessageRenderer mr = (MessageRenderer) factory.getBean("renderer");
         mr.render();
     }
 }
--- src/main/java/ua/aleks4ay/example_1/MessageRenderer.java
+++ src/main/java/ua/aleks4ay/example_1/MessageRenderer.java
@@ -1,8 +1,5 @@
 package ua.aleks4ay.example_1;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class MessageRenderer {
     private String mass = "Hello world";
 
