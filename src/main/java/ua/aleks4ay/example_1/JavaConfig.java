37554aa56eaedb3433c4fa137f66cc3a3f0ab150
Aleksey <aleks4ay@gmail.com>
2021-05-04 15:23:27 +0300

    Using file with properties (@PropertySources) and instance of Environment (from package org.springframework.core.env.*)

--- src/main/java/ua/aleks4ay/example_1/JavaConfig.java
+++ src/main/java/ua/aleks4ay/example_1/JavaConfig.java
@@ -1,11 +1,22 @@
 package ua.aleks4ay.example_1;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@ImportResource(value="classpath:META-INF/spring/app-context-xml.xml")
+@PropertySource(value="classpath:message.properties", encoding = "UTF-8")
+@ComponentScan(basePackages={"ua.aleks4ay.example_1"})
+@EnableTransactionManagement
 public class JavaConfig {
+
+    private Environment env;
+
     @Bean
+    @Scope(value="prototype")
+    @DependsOn(value="messageProvider")
     public MessageRenderer renderer(){
         MessageRenderer messageRenderer = new MessageRenderer();
         messageRenderer.setMessageProvider(messageProvider());
@@ -13,7 +24,13 @@ public class JavaConfig {
     }
 
     @Bean
+    @Lazy
     public MessageProvider messageProvider() {
-        return new MessageProvider("Привет с большой земли из Java-конфига!");
+        return new MessageProvider(env.getProperty("mess"));
+    }
+
+    @Autowired
+    public void setEnv(Environment env) {
+        this.env = env;
     }
 }
